b81fa76e8aab03c7b3a803b07b10de8b9ee0a1c8
mrgallo <daniel.gallo@ycdsbk12.ca>
2018-03-09 02:09:29 +0000

    Reduce runtime output

--- test-mode.sh
+++ test-mode.sh
@@ -22,17 +22,17 @@ update () {
 
         
         if (( NF_V > INT_VERSION )); then 
-            echo "Newer version found."
-            echo "Updating current version."
+            #echo "Newer version found."
+            echo "Updating to latest version."
             cp "$NEW_FILE" "$SCRIPT_NAME"
             rm -f "$NEW_FILE"
             
-            echo "Running updated version..."
+            #echo "Running updated version..."
             bash $SCRIPT_NAME $ARGS
             
             exit 0
         else
-            echo "Current version up to date."
+            #echo "Current version up to date."
             rm -f "$NEW_FILE"
         fi
     }
