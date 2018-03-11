dbdda5c566df904f1d5da9e5529a3139c8822e62
mrgallo <daniel.gallo@ycdsbk12.ca>
2018-03-11 18:28:36 +0000

    testing versioning compare

--- update-robuntu.sh
+++ update-robuntu.sh
@@ -8,37 +8,33 @@ AUTHOR="Mr. Gallo"
 
 TMP_FILE="/tmp/$SCRIPT_NAME"
 LEVEL_FILE=~/.robuntu_update_level
+CURRENT_LEVEL=$(head -1 $LEVEL_FILE)
 ARG1="$1"
 ARG2="$2"
 
 main() {
-    echo "UpdateRobuntu v$VERSION.$REVISION of $DATE, by $AUTHOR."
-    echo
+    case "$ARG1" in
+        "-set-level") set_level    ;;
+            "-level") get_level    ;;
+            
+          "-version") ;&
+                "-v") get_version  ;;
+                
+         "-revision") ;&
+                "-r") get_revision ;;
+                
+                "-h") ;&
+             "-help") show_help    ;;
+    esac
     
-    install
+    show_header
+    
+    #TODO enable 
+    #install
     update
-    
-    CURRENT_LEVEL=$(head -1 $LEVEL_FILE)
-    
-    if [ "$ARG1" = "-set-level" ]; then
-        if [ "$ARG2" != "" ]; then
-            echo "Setting Update Level to $ARG2"
-            echo "$ARG2" > "$LEVEL_FILE"
-        else
-            echo "Error: Need to specify a level to set."
-            echo "E.g., update-robuntu -set-level 3"
-        fi
-        exit 0
-    fi
-    
-    if [ "$ARG1" = "-level" ]; then
-        echo "Current Update Level: $CURRENT_LEVEL"
-        exit 0
-    fi
     
     [ "$ARG1" = "-help" ] || [ "$ARG1" = "-h" ] && show_help
 
-    
     DO_LEVEL=$((CURRENT_LEVEL + 1))
     
     case "$DO_LEVEL" in
@@ -73,6 +69,37 @@ installTestModeScript_20180309() {
     echo "... Done!"
 }
 
+set_level() {
+    if [ "$ARG2" != "" ]; then
+        echo "Setting Update Level to $ARG2"
+        echo "$ARG2" > "$LEVEL_FILE"
+    else
+        echo "Error: Need to specify a level to set."
+        echo "E.g., update-robuntu -set-level 3"
+    fi
+    exit 0
+}
+
+get_level() {
+    echo "Current Update Level: $CURRENT_LEVEL"
+    exit 0
+}
+
+get_version() {
+    echo "$VERSION"
+    exit 0
+}
+
+get_revision() {
+    echo "$REVISION"
+    exit 0
+}
+
+show_header() {
+    echo "UpdateRobuntu v$VERSION.$REVISION of $DATE, by $AUTHOR."
+    echo
+}
+
 do_update () {
     $1  # run update
 
@@ -82,6 +109,8 @@ do_update () {
 }
 
 show_help() {
+    show_header
+    
     echo "Usage: update-robuntu [-options] [args]"
     echo "options:"
     echo "    -help, -h           Help screen"
@@ -107,18 +136,15 @@ install () {
 update () {
     # download most recent version
     
-    wget -qO /tmp/"$SCRIPT_NAME" "https://raw.githubusercontent.com/MrGallo/bash-scripts/master/$SCRIPT_NAME" && {
-        tmpFileV=$(head -4 $TMP_FILE | tail -1)
-        tmpFileR=$(head -5 $TMP_FILE | tail -1)
-        
-        tmpFileV="${tmpFileV//[^0-9]/}"
-        tmpFileV=$((10#$tmpFileV))
-        
-        tmpFileR="${tmpFileR//[^0-9]/}"
-        tmpFileR=$((10#$tmpFileR))
+    wget -O /tmp/"$SCRIPT_NAME" "https://raw.githubusercontent.com/MrGallo/bash-scripts/master/$SCRIPT_NAME" && {
+        v="$(bash "$TMP_FILE" -v)"
+        r="$(bash "$TMP_FILE" -r)"
+        tmpFileV="${v//[^0-9]/}"
+        tmpFileR="${r//[^0-9]/}"
         
         tmpFileVersion=$(( $tmpFileV * 1000 + $tmpFileR ))
         currentVersion=$(( $VERSION * 1000 + $REVISION ))
+        
         
         if (( tmpFileVersion > currentVersion )); then 
             #echo "Newer version found."
@@ -126,7 +152,11 @@ update () {
             cp "$TMP_FILE" "$0"
             rm -f "$TMP_FILE"
             
-            #echo "Running updated version..."
+            # TODO disable
+            echo "Running updated version..."
+            exit 0
+            #---
+            
             bash $0 $ARG1 $ARG2
             exit 0
         else
