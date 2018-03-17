097374572a77e3a7537cefd40219422ebbd634c8
mrgallo <daniel.gallo@ycdsbk12.ca>
2018-03-17 01:08:27 +0000

    Fix github link

--- robuntu-install.sh
+++ robuntu-install.sh
@@ -14,7 +14,7 @@ ARG1="$1"
 
 install() {
     if [ ! -f $FILE_PATH$SCRIPT_NAME ]; then
-        sudo wget -O "$FILE_PATH$SCRIPT_NAME" "https://raw.githubusercontent.com/MrGallo/robuntu_admin/master/$SCRIPT_NAME"
+        sudo wget -O "$FILE_PATH$SCRIPT_NAME" "https://raw.githubusercontent.com/MrGallo/robuntu-admin/master/$SCRIPT_NAME"
         sudo echo "alias robuntu-install='bash $SCRIPT_NAME'" >> "$ALIAS_FILE"
         
         [[ SHUNIT_TRUE ]] && exit 0
