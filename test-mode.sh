b4f2e87a9dfe10adeea7d028b00e51db144b372a
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2018-03-29 11:29:12 -0400

    Update test-mode.sh

--- test-mode.sh
+++ test-mode.sh
@@ -3,7 +3,7 @@
 SCRIPT_NAME=`basename "$0"`
 ARGS="$@"
 NEW_FILE="/tmp/$SCRIPT_NAME"
-VERSION="1.0.2"
+VERSION="1.0.3"
 DATE="March 9 2018"
 AUTHOR="Mr. Gallo"
 
@@ -55,9 +55,9 @@ doStart() {
     sudo git commit -m "Pre-test save"
 
     #    change taskbar alpha
-    xfconf-query -c xfce4-panel -p /panels/panel-1/background-alpha -s 0
+    gksudo -u robuntu "xfconf-query -c xfce4-panel -p /panels/panel-1/background-alpha -s 0"
     # turn off background image
-    xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/image-style -s 0
+    gksudo -u robuntu "xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/image-style -s 0"
     
     # Enable Internet blocking
     sudo wget -qO /etc/chromium-browser/policies/managed/URLBlacklist.json "https://raw.githubusercontent.com/MrGallo/robuntu-admin/master/test-mode/URLBlacklist.json"
@@ -78,9 +78,9 @@ doStop() {
     sudo git add -A && sudo git stash -a && sudo git stash drop
 
     #    change taskbar alpha
-    xfconf-query -c xfce4-panel -p /panels/panel-1/background-alpha -s 100
+    gksudo -u robuntu "xfconf-query -c xfce4-panel -p /panels/panel-1/background-alpha -s 100"
     # turn off background image
-    xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/image-style -s 5
+    gksudo -u robuntu "xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/image-style -s 5"
     
     # Disable Internet blocking
     sudo rm -f /etc/chromium-browser/policies/managed/URLBlacklist.json
