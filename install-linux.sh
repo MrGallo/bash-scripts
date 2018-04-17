768222988fdd8d575b3347098f26508eca6ea6c3
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2018-04-17 13:33:11 -0400

    Update install-linux.sh

--- install-linux.sh
+++ install-linux.sh
@@ -5,14 +5,12 @@ PROVISION_FILE="provision-test.sh"
 
 # install xenial image
 # duration: 10 min
-echo "robuntu
-robuntu
-robuntu" | sudo sh ~/Downloads/crouton -r "$DIST" -t xfce,touch,extension
+sudo sh ~/Downloads/crouton -r "$DIST" -t xfce,touch,extension
 
 # run provision.sh in chroot 
 # TODO: (download file from github)
 # cat provision.sh | sudo enter-chroot
-curl -o ~/Downloads/"$PROVISION_FILE" https://raw.githubusercontent.com/MrGallo/robuntu-admin/master/"$PROVISION_FILE"
-cat ~/Downloads/"$PROVISION_FILE" | sudo enter-chroot
+curl -O https://raw.githubusercontent.com/MrGallo/robuntu-admin/master/$PROVISION_FILE
+cat "$PROVISION_FILE" | sudo enter-chroot
 
 
