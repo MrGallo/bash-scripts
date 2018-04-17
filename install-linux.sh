23f66ae2e5aad66bfd4329f03e187ed677d5ccc1
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2018-04-17 13:46:47 -0400

    Update install-linux.sh

--- install-linux.sh
+++ install-linux.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# 1. Save to downloads folder and run it from there.
+# sudo sh install-linux.sh
+# 2. After linux installs, you have to enter the username and password (twice).
+# enter `robuntu` for username and `robuntu` for password.
+
 DIST="xenial"
 PROVISION_FILE="provision-test.sh"
 
