fd1e025a279eeddfe417814a1eab12331708e219
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2018-04-17 15:14:59 -0400

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -13,16 +13,18 @@ sudo apt-get install file-roller gedit software-center chromium-browser ttf-ubun
 # CONFIRMED /\___________________________/\
 
 # Java 8 
+# wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-x64.tar.gz
+
 # sudo add-apt-repository -y ppa:webupd8team/java
 # sudo apt-get update
 # echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 # sudo apt-get install -y oracle-java8-installer
 # silent
-sudo apt-get install -y python-software-properties debconf-utils
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo apt-get update
-echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-sudo apt-get install -y oracle-java8-installer
+# sudo apt-get install -y python-software-properties debconf-utils
+# sudo add-apt-repository -y ppa:webupd8team/java
+# sudo apt-get update
+# echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+# sudo apt-get install -y oracle-java8-installer
 
 # Python 3.6
 sudo add-apt-repository ppa:deadsnakes/ppa
