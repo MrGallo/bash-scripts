7856e96fe579ae9230e52342e58dd9d4542273a0
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2018-04-25 12:18:17 -0400

    Update crouton remote location

--- install-linux.sh
+++ install-linux.sh
@@ -10,7 +10,7 @@ PROVISION_FILE="provision-test.sh"
 
 # install xenial image
 # duration: 10 min
-curl -O https://goo.gl/fd3zc
+curl -O https://github.com/dnschneid/crouton/raw/master/installer/crouton
 sudo sh crouton -r "$DIST" -t xfce,touch,extension
 
 # run provision.sh in chroot 
