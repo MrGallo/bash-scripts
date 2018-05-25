68afaa37c65929f91336b20e6ba2d06815e382b7
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2018-05-25 09:46:09 -0400

    Update provision-test.sh

--- provision-test.sh
+++ provision-test.sh
@@ -23,20 +23,26 @@ echo "robuntu" | sudo -S echo "Begin image provisioning"
 
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install file-roller git curl -y
+sudo apt-get install git curl -y
 
 XFCE_FILES=(
+  'user-dirs.dirs'
   'xfce4/panel/launcher-1/15197561291.desktop'
   'xfce4/panel/launcher-11/15197525502.desktop'
   'xfce4/panel/launcher-12/15197526083.desktop'
   'xfce4/panel/launcher-10/15197382572.desktop'
   'xfce4/panel/launcher-8/15197524891.desktop'
   'xfce4/panel/launcher-9/15197382571.desktop'
+  'xfce4/xfconf/xfce-perchannel-xml/thunar.xml'
+  'xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml'
+  'xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml'
   'xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml'
   'xfce4/xfconf/xfce-perchannel-xml/xsettings.xml'
   'xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml'
   'xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml'
 )
+
+
 
 installer_output "Downloading xfce4 desktop settings and launcher"
 
